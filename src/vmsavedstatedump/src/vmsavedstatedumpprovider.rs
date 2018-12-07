112bb33986bbacc59c6d59a9b0f5ea94cef4ae16
rafawo <rafawo1@hotmail.com>
2018-12-06 20:31:15 -0800

    Make a dumb attempt to implement VmSavedStateDumpProvider::new(bin: &str, vsv: &str) -> Result<VmSavedStateDumpProvider, ErrorCode>

--- src/vmsavedstatedump/src/vmsavedstatedumpprovider.rs
+++ src/vmsavedstatedump/src/vmsavedstatedumpprovider.rs
@@ -1,7 +1,9 @@
 use crate::vmsavedstatedumpdefs::*;
+use crate::vmsavedstatedump::*;
 use crate::windefs::*;
 
 use std::ops;
+use widestring::U16CString;
 
 pub enum ErrorCode {
     Success(HResult),
@@ -28,11 +30,35 @@ enum VmSavedStateFile {
 pub struct VmSavedStateDumpProvider {
     handle: VmSavedStateDumpHandle,
     saved_state: VmSavedStateFile,
-    virtual_processor_count: u32,
 }
 
 impl ops::Drop for VmSavedStateDumpProvider {
     fn drop(&mut self) {
+        unsafe {
+            // We ignore error code on purpose
+            ReleaseSavedStateFiles(self.handle.clone());
+        }
+    }
+}
 
+impl VmSavedStateDumpProvider {
+    pub fn new(bin: &str, vsv: &str) -> Result<VmSavedStateDumpProvider, ErrorCode> {
+        let mut dump_handle: VmSavedStateDumpHandle = std::ptr::null_mut();
+        let result: HResult;
+
+        unsafe {
+            result = LoadSavedStateFiles(
+                U16CString::from_str(bin).unwrap().as_ptr(),
+                U16CString::from_str(vsv).unwrap().as_ptr(),
+                &mut dump_handle);
+        }
+
+        match hresult_to_error_code(&result) {
+            ErrorCode::Success(_) => Ok(VmSavedStateDumpProvider {
+                handle: dump_handle,
+                saved_state: VmSavedStateFile::BinVsv(String::from(bin), String::from(vsv)),
+            }),
+            error => Err(error),
+        }
     }
 }
